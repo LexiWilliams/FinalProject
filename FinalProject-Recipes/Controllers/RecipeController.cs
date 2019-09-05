f09360f956ac44b91e07409f8b9fe5b808160b59
Lexi <lexa.shelby@gmail.com>
2019-09-05 16:50:03 -0400

    first commit

---
+++ FinalProject-Recipes/Controllers/RecipeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FinalProject_Recipes.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace FinalProject_Recipes.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _apiKey;
+        public RecipeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _apiKey = _configuration.GetSection("AppConfiguration")["RecipeAPIKey"];
+
+        }
+        public HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.themealdb.com");
+            return client;
+        }
+        public async Task<Recipe> GetFlights()
+        {
+            var client = GetHttpClient();
+
+            var response = await client.GetAsync($"api/json/v1/{_apiKey}/random.php");
+            var meal = await response.Content.ReadAsAsync<Recipe>();
+            return meal;
+        }
+        public IActionResult Index()
+        {
+            var meal = GetFlights().Result;
+            return View(meal);
+        }
+    }
+}
