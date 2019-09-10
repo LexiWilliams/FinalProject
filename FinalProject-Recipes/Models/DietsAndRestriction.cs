d7d65ee76b010c9d289d7706f5023032155c8499
Lexi <lexa.shelby@gmail.com>
2019-09-10 16:29:28 -0400

    fixed data tables

--- FinalProject-Recipes/Models/DietsAndRestriction.cs
+++ FinalProject-Recipes/Models/DietsAndRestriction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace FinalProject_Recipes.Models
 {
-    public partial class IngredientList
+    public partial class DietsAndRestriction
     {
         public string Id { get; set; }
         public bool? Vegan { get; set; }
@@ -12,5 +12,13 @@ namespace FinalProject_Recipes.Models
         public bool? Paleo { get; set; }
         public bool? GlutenFree { get; set; }
         public bool? Pescatarian { get; set; }
+        public bool? Milk { get; set; }
+        public bool? Eggs { get; set; }
+        public bool? Shellfish { get; set; }
+        public bool? Fish { get; set; }
+        public bool? Soy { get; set; }
+        public bool? Wheat { get; set; }
+        public bool? Treenuts { get; set; }
+        public bool? Peaunts { get; set; }
     }
 }
