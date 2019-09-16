bc475ff4fe87f019018c8e85e30e6e1584c419a1
Deno <denobrockert@yahoo.com>
2019-09-16 16:28:38 -0400

    Added navigation between group views and fixed the search function so it will take you back to that group

--- FinalProject-Recipes/Controllers/GroupController.cs
+++ FinalProject-Recipes/Controllers/GroupController.cs
@@ -61,6 +61,7 @@ namespace FinalProject_Recipes.Controllers
                 var memberInfo = _context.AspNetUsers.Where(u => u.Id == member.UserId).First();
                 groupList.Add(memberInfo);
             }
+            TempData["Name"] = group.GroupName;
             return View(groupList);
 
         }
@@ -143,91 +144,7 @@ namespace FinalProject_Recipes.Controllers
             return RedirectToAction("DisplayGroups");
         }
 
-        //public List<string> AddIngredients(Meal item)
-        //{
-        //    var ingredients = new List<string>();
-        //    if (item.strIngredient1 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient1.ToLower());
-        //    }
-        //    if (item.strIngredient2 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient2.ToLower());
-        //    }
-        //    if (item.strIngredient3 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient3.ToLower());
-        //    }
-        //    if (item.strIngredient4 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient4.ToLower());
-        //    }
-        //    if (item.strIngredient5 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient5.ToLower());
-        //    }
-        //    if (item.strIngredient6 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient6.ToLower());
-        //    }
-        //    if (item.strIngredient7 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient7.ToLower());
-        //    }
-        //    if (item.strIngredient8 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient8.ToLower());
-        //    }
-        //    if (item.strIngredient9 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient9.ToLower());
-        //    }
-        //    if (item.strIngredient10 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient10.ToLower());
-        //    }
-        //    if (item.strIngredient11 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient11.ToLower());
-        //    }
-        //    if (item.strIngredient12 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient12.ToLower());
-        //    }
-        //    if (item.strIngredient13 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient13.ToLower());
-        //    }
-        //    if (item.strIngredient14 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient14.ToLower());
-        //    }
-        //    if (item.strIngredient15 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient15.ToLower());
-        //    }
-        //    if (item.strIngredient16 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient16.ToLower());
-        //    }
-        //    if (item.strIngredient17 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient17.ToLower());
-        //    }
-        //    if (item.strIngredient18 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient18.ToLower());
-        //    }
-        //    if (item.strIngredient19 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient19.ToLower());
-        //    }
-        //    if (item.strIngredient20 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient20.ToLower());
-        //    }
-        //    return ingredients;
-        //}
+      
 
         public List<DietsAndRestriction> GetAllergies()
         {
@@ -332,15 +249,16 @@ namespace FinalProject_Recipes.Controllers
             //diet.AddRange(allergies);
             return allergies;
         }
-        public async Task<IActionResult> SearchRecipesTitle(string search)
+        public async Task<IActionResult> SearchRecipesTitle(string search,string groupName)
         {
             var client = RecipeMethods.GetHttpClient();
             var response = await client.GetAsync($"api/json/v1/{_apiKey}/search.php?s={search}");
             var recipes = await response.Content.ReadAsAsync<Recipe>();
             if (recipes.meals == null)
             {
-                TempData["RegexMatch"] = "Please enter a valid search";
-                return RedirectToAction("DisplayGroups");
+                Group group = _context.Group.Where(u => u.GroupName == groupName).First();
+                //TempData["RegexMatch"] = "Please enter a valid search";
+                return RedirectToAction("ViewGroup","Group", group);
             }
             else
             {
