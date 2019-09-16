81070de0952830118c3051d0f96eb25f2b7e07d1
Lexi <lexa.shelby@gmail.com>
2019-09-16 11:50:52 -0400

    added links and images for area search on homepage

--- FinalProject-Recipes/Controllers/RecipeController.cs
+++ FinalProject-Recipes/Controllers/RecipeController.cs
@@ -148,9 +148,11 @@ namespace FinalProject_Recipes.Controllers
             var client = RecipeMethods.GetHttpClient();
             var response = await client.GetAsync($"api/json/v1/{_apiKey}/filter.php?a={search}");
             var recipes = await response.Content.ReadAsAsync<Recipe>();
-            var filteredRecipes = FilterRecipes(recipes);
+            
+                var actualRecipes = GetRecipesById(recipes);
+                var filteredRecipes = FilterRecipes(actualRecipes);
 
-            return View("FindRecipes", filteredRecipes);
+                return View("FindRecipes", filteredRecipes);
         }
         
         public async Task<Meal> GetRecipeByName(string meal)
@@ -169,91 +171,6 @@ namespace FinalProject_Recipes.Controllers
             return viewMeal;
         }
 
-        //public List<string> AddIngredients(Meal item)
-        //{
-        //    var ingredients = new List<string>();
-        //    if (item.strIngredient1 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient1.ToLower());
-        //    }
-        //    if (item.strIngredient2 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient2.ToLower());
-        //    }
-        //    if (item.strIngredient3 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient3.ToLower());
-        //    }
-        //    if (item.strIngredient4 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient4.ToLower());
-        //    }
-        //    if (item.strIngredient5 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient5.ToLower());
-        //    }
-        //    if (item.strIngredient6 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient6.ToLower());
-        //    }
-        //    if (item.strIngredient7 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient7.ToLower());
-        //    }
-        //    if (item.strIngredient8 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient8.ToLower());
-        //    }
-        //    if (item.strIngredient9 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient9.ToLower());
-        //    }
-        //    if (item.strIngredient10 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient10.ToLower());
-        //    }
-        //    if (item.strIngredient11 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient11.ToLower());
-        //    }
-        //    if (item.strIngredient12 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient12.ToLower());
-        //    }
-        //    if (item.strIngredient13 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient13.ToLower());
-        //    }
-        //    if (item.strIngredient14 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient14.ToLower());
-        //    }
-        //    if (item.strIngredient15 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient15.ToLower());
-        //    }
-        //    if (item.strIngredient16 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient16.ToLower());
-        //    }
-        //    if (item.strIngredient17 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient17.ToLower());
-        //    }
-        //    if (item.strIngredient18 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient18.ToLower());
-        //    }
-        //    if (item.strIngredient19 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient19.ToLower());
-        //    }
-        //    if (item.strIngredient20 != null)
-        //    {
-        //        ingredients.Add(item.strIngredient20.ToLower());
-        //    }
-        //    return ingredients;
-        //}
         public List<DietsAndRestriction> GetDiet()
         {
             if (User.Identity.Name != null)
